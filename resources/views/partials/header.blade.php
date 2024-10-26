f6b99de14346330205c5c70dd423096e2729b13c
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-26 11:58:06 -0400

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,8 @@
 
-<header>
-    
+<header class="mb-5">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Information Trains</span>
+        </div>
+      </nav>
 </header>
